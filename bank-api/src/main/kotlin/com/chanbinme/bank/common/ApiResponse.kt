b19361939eb48dcfad2eb41143193cd4c61efc15
chanbinme <gksmfcksqls@gmail.com>
2025-10-28 23:10:22 +0900

    feat: API Response 규격화

---
+++ bank-api/src/main/kotlin/com/chanbinme/bank/common/ApiResponse.kt
@@ -0,0 +1,34 @@
+package com.chanbinme.bank.common
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.springframework.http.ResponseEntity
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ApiResponse<T>(
+    val success: Boolean,
+    val message: String,
+    val data: T? = null,
+    val err: Error? = null
+) {
+    companion object {
+        fun <T> success(data: T, msg: String = "Success"): ResponseEntity<ApiResponse<T>> {
+            return ResponseEntity.ok(ApiResponse(true, msg, data))
+        }
+
+        fun <T> error(
+            msg: String,
+            errCode: String? = null,
+            details: Any? = null,
+            path: String? = null
+        ) : ResponseEntity<ApiResponse<T>> {
+            val error = Error(errCode, details, path)
+            return ResponseEntity.badRequest().body(ApiResponse(false, msg, null, error))
+        }
+    }
+}
+
+data class Error(
+    val code: String? = null,
+    val details: Any? = null,
+    val path: String? = null
+)
